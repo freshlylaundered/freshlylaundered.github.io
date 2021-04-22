b7da915fd5f94ad5d7733d75977737229230c855
Patrick LeDuc <jamespatrickleduc@gmail.com>
2021-04-22 15:11:59 -0400

    cname

---
+++ content/blog/fourth/index.md
@@ -0,0 +1,9 @@
+---
+title: The fourth post
+date: "2021-04-22"
+description: "Forth"
+---
+
+Hello and welcome to my fourth blog post. I'm very happy to see you all here today yes hello.
+
+[]
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `James Patrick LeDuc`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Patrick LeDuc`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
