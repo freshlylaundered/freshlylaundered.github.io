6e63d0233f7e0351e63f2ea2a2a4acedeb638e6d
Patrick LeDuc <pleduc@Patricks-Air.hsd1.ma.comcast.net>
2021-04-26 10:34:37 -0400

    rebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `LeDuc Personal Site`,
+    author: `Patrick LeDuc`,
+    description: `A personal site based on Gatsby and Netlify`,
+    siteUrl: `https://jamespatrickleduc.com/`,
     social: {
       twitter: `gatsbyjs`,
     },
